0e4e9681b39678c038359c7f0c58bff225d6e850
Lucas Nieuwenhout <lucasnwnht@gmail.com>
2022-07-21 10:48:22 -0600

    Photo page mostly working, next step is to sort out putting images on the page.

---
+++ photography/photolist.js
@@ -0,0 +1,18 @@
+const data = '{
+  "photos": [
+    {
+      "srcset": "test1",
+      "sizes": "test2",
+      "src": "test3",
+      "alt": "test4",
+      "caption": "test5"
+    },
+    {
+      "srcset": "test6",
+      "sizes": "test7",
+      "src": "test8",
+      "alt": "test9",
+      "caption": "test0"
+    }
+  ]
+}';
